2eeb46ce6fbb25c3a2bccfa3af4c5f100c979e73
Andrea Colledan <and.colledan@gmail.com>
2024-02-12 11:46:26 +0100

    Formatted test files

--- test/ASTSpec/CircuitSpec.hs
+++ test/ASTSpec/CircuitSpec.hs
@@ -2,37 +2,34 @@ module ASTSpec.CircuitSpec (spec) where
 
 import AST.Bundle
 import AST.Circuit
-
 import qualified Data.Map as Map
 import Test.Hspec
 
-
 -- SPECIFICATION --
-
 
 widthSpec :: Spec
 widthSpec = do
-    describe "circuit width" $ do
-        it "matches the size of the label context for identity circuits" $ do
-            -- width(ID_∅) = 0
-            width (Id Map.empty) `shouldBe` 0
-            -- width (ID_{q:Qubit}) = 1
-            width (Id $ Map.fromList [("q",Qubit)]) `shouldBe` 1
-            -- width(ID_{q:Qubit,r:Qubit,s:Qubit}) = 3
-            width (Id $ Map.fromList [("q",Qubit),("r",Qubit),("s",Qubit)]) `shouldBe` 3
-        it "does not increase when an operation other than an init is applied" $ do
-            -- width (ID_{q:Qubit}; H(q) -> q) = 1
-            width (Seq (Id $ Map.fromList[("q",Qubit)]) Discard (Label "q") UnitValue) `shouldBe` 1
-            -- width (ID_{q:Qubit}; H(q) -> q; X(q) -> q) = 1
-            width (Seq (Seq (Id $ Map.fromList [("q",Qubit)]) Hadamard (Label "q") (Label "q")) PauliX (Label "q") (Label "q")) `shouldBe` 1
-        it "should increase when an init is applied and no discarded qubits exist" $ do
-            -- width (ID_∅; Init(*) -> q) = 1
-            width (Seq (Id Map.empty) Init UnitValue (Label "q")) `shouldBe` 1
-            -- width (ID_{q:Qubit}; H(q) -> q; Init(*) -> r) = 2
-            width (Seq (Seq (Id $ Map.fromList [("q",Qubit)]) Hadamard (Label "q") (Label "q")) Init UnitValue (Label "r")) `shouldBe` 2
-            -- width (ID_∅; Init(*) -> q; Init(*) -> r) = 2
-            width (Seq (Seq (Id Map.empty) Init UnitValue (Label "q")) Init UnitValue (Label "r")) `shouldBe` 2
+  describe "circuit width" $ do
+    it "matches the size of the label context for identity circuits" $ do
+      -- width(ID_∅) = 0
+      width (Id Map.empty) `shouldBe` 0
+      -- width (ID_{q:Qubit}) = 1
+      width (Id $ Map.fromList [("q", Qubit)]) `shouldBe` 1
+      -- width(ID_{q:Qubit,r:Qubit,s:Qubit}) = 3
+      width (Id $ Map.fromList [("q", Qubit), ("r", Qubit), ("s", Qubit)]) `shouldBe` 3
+    it "does not increase when an operation other than an init is applied" $ do
+      -- width (ID_{q:Qubit}; H(q) -> q) = 1
+      width (Seq (Id $ Map.fromList [("q", Qubit)]) Discard (Label "q") UnitValue) `shouldBe` 1
+      -- width (ID_{q:Qubit}; H(q) -> q; X(q) -> q) = 1
+      width (Seq (Seq (Id $ Map.fromList [("q", Qubit)]) Hadamard (Label "q") (Label "q")) PauliX (Label "q") (Label "q")) `shouldBe` 1
+    it "should increase when an init is applied and no discarded qubits exist" $ do
+      -- width (ID_∅; Init(*) -> q) = 1
+      width (Seq (Id Map.empty) Init UnitValue (Label "q")) `shouldBe` 1
+      -- width (ID_{q:Qubit}; H(q) -> q; Init(*) -> r) = 2
+      width (Seq (Seq (Id $ Map.fromList [("q", Qubit)]) Hadamard (Label "q") (Label "q")) Init UnitValue (Label "r")) `shouldBe` 2
+      -- width (ID_∅; Init(*) -> q; Init(*) -> r) = 2
+      width (Seq (Seq (Id Map.empty) Init UnitValue (Label "q")) Init UnitValue (Label "r")) `shouldBe` 2
 
 spec :: Spec
 spec = do
-    widthSpec
+  widthSpec
--- test/ParsingSpec/IndexSpec.hs
+++ test/ParsingSpec/IndexSpec.hs
@@ -1,26 +1,27 @@
-module ParsingSpec.IndexSpec (
-    spec
-) where
+module ParsingSpec.IndexSpec
+  ( spec,
+  )
+where
 
-import Test.Hspec
-import Text.Parsec (parse)
 import AST.Index
 import Parsing.Index
+import Test.Hspec
+import Text.Parsec (parse)
 
 spec :: Spec
 spec = do
-    describe "index parser" $ do
-        it "parses a natural number" $ do
-            parse parseIndex "" "123" `shouldBe` Right (Number 123)
-        it "parses an index variable" $ do
-            parse parseIndex "" "a" `shouldBe` Right (IndexVariable "a")
-        it "parses a max" $ do
-            parse parseIndex "" "max(1,2)" `shouldBe` Right (Max (Number 1) (Number 2))
-        it "parses a sum" $ do
-            parse parseIndex "" "1+2" `shouldBe` Right (Plus (Number 1) (Number 2))
-        it "parses a product" $ do
-            parse parseIndex "" "1*2" `shouldBe` Right (Mult (Number 1) (Number 2))
-        it "parses a maximum" $ do
-            parse parseIndex "" "max[i<4] (i*2 + 2)" `shouldBe` Right (Maximum "i" (Number 4) (Plus (Mult (IndexVariable "i") (Number 2)) (Number 2)))
-        it "parses complex expressions" $ do
-            parse parseIndex "" "max[j<k](max(1, max[i<j] (i+1 + (j-1-i) * 2)))" `shouldBe` Right (Maximum "j" (IndexVariable "k") (Max (Number 1) (Maximum "i" (IndexVariable "j") (Plus (Plus (IndexVariable "i") (Number 1)) (Mult (Minus (Minus (IndexVariable "j") (Number 1)) (IndexVariable "i")) (Number 2))))))
+  describe "index parser" $ do
+    it "parses a natural number" $ do
+      parse parseIndex "" "123" `shouldBe` Right (Number 123)
+    it "parses an index variable" $ do
+      parse parseIndex "" "a" `shouldBe` Right (IndexVariable "a")
+    it "parses a max" $ do
+      parse parseIndex "" "max(1,2)" `shouldBe` Right (Max (Number 1) (Number 2))
+    it "parses a sum" $ do
+      parse parseIndex "" "1+2" `shouldBe` Right (Plus (Number 1) (Number 2))
+    it "parses a product" $ do
+      parse parseIndex "" "1*2" `shouldBe` Right (Mult (Number 1) (Number 2))
+    it "parses a maximum" $ do
+      parse parseIndex "" "max[i<4] (i*2 + 2)" `shouldBe` Right (Maximum "i" (Number 4) (Plus (Mult (IndexVariable "i") (Number 2)) (Number 2)))
+    it "parses complex expressions" $ do
+      parse parseIndex "" "max[j<k](max(1, max[i<j] (i+1 + (j-1-i) * 2)))" `shouldBe` Right (Maximum "j" (IndexVariable "k") (Max (Number 1) (Maximum "i" (IndexVariable "j") (Plus (Plus (IndexVariable "i") (Number 1)) (Mult (Minus (Minus (IndexVariable "j") (Number 1)) (IndexVariable "i")) (Number 2))))))
